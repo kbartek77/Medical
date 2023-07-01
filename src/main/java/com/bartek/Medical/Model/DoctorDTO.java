97626d1488efd6c7041affe62e1bae09de59b65b
Bartek <bartekkrol2003@gmail.com>
2023-07-01 17:54:47 +0200

    Updated MED-99

--- src/main/java/com/bartek/Medical/Model/DoctorDTO.java
+++ src/main/java/com/bartek/Medical/Model/DoctorDTO.java
@@ -1,10 +1,16 @@
 package com.bartek.Medical.Model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 
 @Data
 public class DoctorDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
     private String firstName;
     private String lastName;
     private String specialization;

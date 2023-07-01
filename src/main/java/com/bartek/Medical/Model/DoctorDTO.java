77d3614c933ec8be8f8caf4375f8cccfefb88202
Bartek <bartekkrol2003@gmail.com>
2023-07-01 17:40:35 +0200

    Updated MED-99

---
+++ src/main/java/com/bartek/Medical/Model/DoctorDTO.java
@@ -0,0 +1,10 @@
+package com.bartek.Medical.Model;
+
+import lombok.Data;
+
+@Data
+public class DoctorDTO {
+    private String fristName;
+    private String lastName;
+    private String specialization;
+}

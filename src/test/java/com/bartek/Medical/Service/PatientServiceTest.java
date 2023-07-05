719d83d906f585405f4e905af1641f5ed55ae1c2
Bartek <bartekkrol2003@gmail.com>
2023-07-05 16:56:59 +0200

    added feature test

--- src/test/java/com/bartek/Medical/Service/PatientServiceTest.java
+++ src/test/java/com/bartek/Medical/Service/PatientServiceTest.java
@@ -1,5 +1,7 @@
 package com.bartek.Medical.Service;
 
+import com.bartek.Medical.Exception.InvalidEmailException;
+import com.bartek.Medical.Exception.PatientNotFoundException;
 import com.bartek.Medical.Mapper.PatientMapper;
 import com.bartek.Medical.Model.Patient;
 import com.bartek.Medical.Model.PatientDto;
@@ -13,14 +15,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PatientServiceTest {
@@ -36,78 +33,145 @@ public class PatientServiceTest {
         List<Patient> patients = new ArrayList<>();
         patients.add(new Patient(1L, "bartek123@gmail.com", "bartek123", "123", "Bartek", "Kkk", "12343567", LocalDate.of(2000, 10, 10)));
         patients.add(new Patient(2L, "Dawid@gmail.com", "Dawid123", "1234", "Dawid", "Xxx", "98751513", LocalDate.of(1990, 1, 1)));
+
         when(patientRepository.findAll()).thenReturn(patients);
-        when(patientMapper.toDtoList(patients)).thenReturn(Arrays.asList(
-                new PatientDto(),
-                new PatientDto()
-        ));
+        when(patientMapper.toDtoList(patients)).thenReturn(Arrays.asList(new PatientDto(), new PatientDto()));
 
         List<PatientDto> result = patientService.getAllPatients();
-        var result1 = patientService.getAllPatients();
+
         Assertions.assertEquals(2, result.size());
     }
 
     @Test
-    void GetPatientByEmail_patientExist_returnPatientByEmail() {
+    void getAllPatients_NoPatientsInDatabase_EmptyListReturned() {
+        when(patientRepository.findAll()).thenReturn(Collections.emptyList());
+
+        List<PatientDto> result = patientService.getAllPatients();
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void getPatientByEmail_patientExist_returnPatientByEmail() {
         String email = "bartek123@gmail.com";
         Patient patient = new Patient(1L, "bartek123@gmail.com", "bartek123", "123", "Bartek", "Kkk", "12343567", LocalDate.of(2000, 10, 10));
         when(patientRepository.findByEmail(email)).thenReturn(Optional.of(patient));
+
         PatientDto expectedDto = new PatientDto();
         when(patientMapper.toDto(patient)).thenReturn(new PatientDto());
         PatientDto result = patientService.getPatientByEmail(email);
+
         Assertions.assertNotNull(result);
         Assertions.assertEquals(expectedDto, result);
 
     }
+
     @Test
-    void AddPatient_Success() {
+    void getPatientByEmail_PatientNotFound_ExceptionThrown() {
+        String email = "nonexistent@example.com";
+        when(patientRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        var exception = Assertions.assertThrows(PatientNotFoundException.class, () -> patientService.getPatientByEmail(email));
+        Assertions.assertEquals("Pacjent o podanym adresie e-mail nie został odnaleziony.", exception.getMessage());
+    }
+
+    @Test
+    void addPatient_PatientExist_PatientAdded() {
         PatientDto patientDto = new PatientDto();
         patientDto.setEmail("Dawid@gmail.com");
         patientDto.setFirstName("Dawid");
         patientDto.setLastName("Xxx");
 
-        when(patientRepository.findByEmail(patientDto.getEmail())).thenReturn(Optional.empty());
+        when(patientRepository.findByEmail(eq(patientDto.getEmail()))).thenReturn(Optional.empty());
 
         PatientDto result = patientService.addPatient(patientDto);
 
-        Assertions.assertEquals(patientDto, result);
+        Assertions.assertEquals(patientDto.getEmail(), result.getEmail());
+        verify(patientRepository, times(1)).save(any());
     }
+
     @Test
-    void testDeletePatientByEmail_Successful() {
+    void addPatient_PatientWithEmailAlreadyExists_ExceptionThrown() {
+        PatientDto patientDto = new PatientDto();
+        patientDto.setEmail("existing@example.com");
+
+        when(patientRepository.findByEmail(patientDto.getEmail())).thenReturn(Optional.of(new Patient()));
+
+        var exception = Assertions.assertThrows(InvalidEmailException.class, () -> patientService.addPatient(patientDto));
+        Assertions.assertEquals("Pacjent o podanym adresie e-mail już istnieje.", exception.getMessage());
+    }
+
+    @Test
+    void testDeletePatientByEmail_PatientExist_PatientDeleted() {
         String email = "Dawid@gmail.com";
         Patient patient = new Patient(2L, "Dawid@gmail.com", "Dawid123", "1234", "Dawid", "Xxx", "98751513", LocalDate.of(1990, 1, 1));
-        when(patientRepository.findByEmail(email)).thenReturn(Optional.of(patient));
+
+        when(patientRepository.findByEmail(eq(email))).thenReturn(Optional.of(patient));
         boolean result = patientService.deletePatientByEmail(email);
+
         Assertions.assertTrue(result);
+        verify(patientRepository, times(1)).delete(eq(patient));
 
     }
+
     @Test
-    void UpdatePatient_Successful() {
+    void deletePatientByEmail_PatientNotFound_ExceptionThrown() {
+        String email = "nonexistent@example.com";
+        when(patientRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        var exception = Assertions.assertThrows(PatientNotFoundException.class, () -> patientService.deletePatientByEmail(email));
+        Assertions.assertEquals("Pacjent o podanym adresie e-mail nie został odnaleziony.", exception.getMessage());
+    }
+
+    @Test
+    void updatePatient_DataCorret_DataUpdated() {
         String email = "Bartek123@gmail.com";
         PatientDto updatedPatientDto = new PatientDto();
         updatedPatientDto.setEmail("Bartek123@gmail.com");
         updatedPatientDto.setFirstName("Bartek");
-        updatedPatientDto.setLastName("Xxx");
-        Patient existingPatient = new Patient(1L, "bartek123@gmail.com", "bartek123", "123", "Bartek", "Kkk", "12343567", LocalDate.of(2000, 10, 10));
+        updatedPatientDto.setLastName("Kkk");
+
+        Patient existingPatient = new Patient(1L, "Bartek123@gmail.com", "bartek123", "123", "Bartek", "Kkk", "12343567", LocalDate.of(2000, 10, 10));
         when(patientRepository.findByEmail(email)).thenReturn(Optional.of(existingPatient));
         patientService.updatePatient(email, updatedPatientDto);
-        Assertions.assertEquals(updatedPatientDto, existingPatient);
+
+        Assertions.assertEquals(updatedPatientDto.getEmail(), existingPatient.getEmail());
+        Assertions.assertEquals(updatedPatientDto.getFirstName(), existingPatient.getFirstName());
+        Assertions.assertEquals(updatedPatientDto.getLastName(), existingPatient.getLastName());
     }
 
     @Test
-    void testUpdatePassword_Successful() {
+    void updatePatient_PatientNotFound_ExceptionThrown() {
+        String email = "Bartek123@gmail.com";
+        PatientDto updatedPatientDto = new PatientDto();
+
+        when(patientRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        var exception = Assertions.assertThrows(PatientNotFoundException.class, () -> patientService.updatePatient(email, updatedPatientDto));
+        Assertions.assertEquals("Pacjent o podanym adresie e-mail nie został odnaleziony.", exception.getMessage());
+    }
+
+    @Test
+    void testUpdatePassword_SetPasswordCorrect_PasswordUpdated() {
         String email = "Bartek123@gmail.com";
         String newPassword = "newpassword";
         Patient existingPatient = new Patient(1L, "bartek123@gmail.com", "bartek123", "123", "Bartek", "Kkk", "12343567", LocalDate.of(2000, 10, 10));
+
         when(patientRepository.findByEmail(email)).thenReturn(Optional.of(existingPatient));
         boolean result = patientService.updatePassword(email, newPassword);
+
         Assertions.assertTrue(result);
         Assertions.assertEquals(newPassword, existingPatient.getPassword());
     }
 
+    @Test
+    void updatePassword_PatientNotFound_ExceptionThrown() {
+        String email = "Dawid@gmail.com";
+        String newPassword = "newpassword";
 
+        when(patientRepository.findByEmail(email)).thenReturn(Optional.empty());
 
-
-
-
+        var exception = Assertions.assertThrows(PatientNotFoundException.class, () -> patientService.updatePassword(email, newPassword));
+        Assertions.assertEquals("Pacjent o podanym adresie e-mail nie został odnaleziony.", exception.getMessage());
+    }
 }

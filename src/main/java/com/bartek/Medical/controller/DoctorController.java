95074e1b6d7d0a0383bfdc2f3c376192ebccb0e3
Bartek <bartekkrol2003@gmail.com>
2023-07-29 12:25:38 +0200

    added hospital service

--- src/main/java/com/bartek/Medical/controller/DoctorController.java
+++ src/main/java/com/bartek/Medical/controller/DoctorController.java
@@ -5,9 +5,15 @@ import com.bartek.Medical.model.DoctorDTO;
 import com.bartek.Medical.model.Hospital;
 import com.bartek.Medical.model.MessageDto;
 import com.bartek.Medical.service.DoctorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,36 +24,66 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DoctorController {
     private final DoctorService doctorService;
-
+    @Operation(summary = "Get All Doctors", tags = "Doctors")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @GetMapping
     public List<DoctorDTO> getAllDoctors() {
         return doctorService.getAllDoctors();
     }
-
+    @Operation(summary = "Get Doctors by id", tags = "Doctors")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<DoctorDTO> getDoctorById(@PathVariable Long id) {
         DoctorDTO doctor = doctorService.getDoctorById(id);
         return ResponseEntity.ok(doctor);
     }
-
+    @Operation(summary = "Add dodctor to data base", tags = "Doctors")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<DoctorDTO> addDoctor(@RequestBody DoctorDTO doctorDTO) {
         DoctorDTO savedDoctor = doctorService.addDoctor(doctorDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedDoctor);
     }
-
+    @Operation(summary = "Delete doctor by id", tags = "Doctors")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity <MessageDto> deleteDoctorById(@PathVariable Long id) {
         MessageDto messageDto = doctorService.deleteDoctorById(id);
         return ResponseEntity.status(messageDto.getHttpStatus()).body(messageDto);
     }
-
+    @Operation(summary = "Update doctor password by id", tags = "Doctors")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = MessageDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @PatchMapping("/{id}/password")
     public ResponseEntity<MessageDto> updatePassword(@PathVariable Long id, @RequestBody String newPassword) {
         MessageDto messageDto = doctorService.updatePassoword(id, newPassword);
         return ResponseEntity.status(messageDto.getHttpStatus()).body(messageDto);
     }
-
+    @Operation(summary = "Add hospital to doctor", tags = "Doctors")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @PostMapping("/{id}/hospitals")
     public ResponseEntity<DoctorDTO> addHospitalToDoctor(@PathVariable Long id, @RequestBody String hospitalName) {
         DoctorDTO doctorWithHospital = doctorService.addHospitalToDoctor(id, hospitalName);
--- src/main/java/com/bartek/Medical/controller/VisitController.java
+++ src/main/java/com/bartek/Medical/controller/VisitController.java
@@ -2,7 +2,13 @@ package com.bartek.Medical.controller;
 
 import com.bartek.Medical.model.VisitDto;
 import com.bartek.Medical.service.VisitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,19 +19,35 @@ import java.util.List;
 @AllArgsConstructor
 public class VisitController {
     private final VisitService visitService;
-
+    @Operation(summary = "Create new visit in database", tags = "Visit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = VisitDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid or taken date", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<VisitDto> createVisit(@RequestBody VisitDto visitDto) {
         VisitDto createdVisit = visitService.createVisit(visitDto);
         return ResponseEntity.ok(createdVisit);
     }
-
+    @Operation(summary = "Assign patient to visit", tags = "Visit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = VisitDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid or taken date", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Visit not found", content = @Content)
+    })
     @PatchMapping("/{visitId}/assign")
     public ResponseEntity<VisitDto> assignPatientToVisit(@PathVariable Long visitId, @RequestBody Long patientId) {
         VisitDto assignedVisit = visitService.assignPatientToVisit(visitId, patientId);
         return ResponseEntity.ok(assignedVisit);
     }
-
+    @Operation(summary = "Get all visits from one patient by id", tags = "Visit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = VisitDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @GetMapping("/patient/{patientId}")
     public ResponseEntity<List<VisitDto>> getPatientVisits(@PathVariable Long patientId) {
         List<VisitDto> patientVisits = visitService.getPatientVisits(patientId);
